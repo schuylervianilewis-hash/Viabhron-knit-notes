9a5037c10d7695881edfaabdad1f1dc4eaebade3
schuylervianilewis-hash <schuylervianilewis@gmail.com>
2026-08-30 23:09:49 +0530

    [2/7] Deploy viabhron-voice-notes via Vian Github Help

---
+++ app/src/main/java/com/example/ui/theme/Color.kt
@@ -0,0 +1,39 @@
+package com.example.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Brand Colors
+val ColorNoteHeader = Color(0xFFFFFFFF)
+val ColorNoteBackground = Color(0xFFF7F9FC)
+val ColorNoteDarkBackground = Color(0xFF121418)
+
+// ColorNote Card Pastel Colors
+val NoteYellow = Color(0xFFFFF9C4)
+val NoteYellowStripe = Color(0xFFFBC02D)
+
+val NotePeach = Color(0xFFFFE0B2)
+val NotePeachStripe = Color(0xFFFB8C00)
+
+val NotePink = Color(0xFFFFCDD2)
+val NotePinkStripe = Color(0xFFE53935)
+
+val NoteGreen = Color(0xFFC8E6C9)
+val NoteGreenStripe = Color(0xFF43A047)
+
+val NoteBlue = Color(0xFFBBDEFB)
+val NoteBlueStripe = Color(0xFF1E88E5)
+
+val NotePurple = Color(0xFFE1BEE7)
+val NotePurpleStripe = Color(0xFF8E24AA)
+
+// LogKeeper Colors
+val LogCardBg = Color(0xFFE2E8F0)
+val LogCardBgDark = Color(0xFF1E293B)
+val LogTagColor = Color(0xFF0F172A)
+val LogTimestampColor = Color(0xFF334155)
+
+// Material You Base
+val PrimaryTeal = Color(0xFF00695C)
+val PrimaryTealContainer = Color(0xFFB2DFDB)
+val SecondaryAmber = Color(0xFFFF8F00)
+val SecondaryAmberContainer = Color(0xFFFFE082)
